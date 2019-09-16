0dc90b7449a3b551cd0a3947a10ead22d5c4280a
mordi34 <mserraf34@gmail.com>
2019-09-16 22:05:38 +0200

    add tranId to advices

--- src/main/java/io/electrum/qr/api/model/PaymentConfirmation.java
+++ src/main/java/io/electrum/qr/api/model/PaymentConfirmation.java
@@ -1,8 +1,12 @@
 package io.electrum.qr.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.electrum.vas.Utils;
 import io.electrum.vas.model.BasicAdvice;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Confirm that a previous {@link PaymentRequest} has completed successfully at the POS.
@@ -10,19 +14,39 @@ import io.swagger.annotations.ApiModel;
 
 @ApiModel(description = "Confirm that a previous {@link PaymentRequest} has completed successfully at the POS.")
 public class PaymentConfirmation extends BasicAdvice {
+    protected String tranId = null;
 
-   @Override
-   public String toString() {
-      StringBuilder sb = new StringBuilder();
-      sb.append("class PaymentConfirmation {\n");
+    /**
+     * The unique transaction identifier related to this transaction. This transaction identifier was encoded within the
+     * QR Code and used to associate the scan and the payment request.
+     **/
+    public PaymentConfirmation tranId(String tranId) {
+        this.tranId = tranId;
+        return this;
+    }
 
-      sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
-      sb.append("    requestId: ").append(Utils.toIndentedString(requestId)).append("\n");
-      sb.append("    thirdPartyIdentifiers: ").append(Utils.toIndentedString(thirdPartyIdentifiers)).append("\n");
-      sb.append("    time: ").append(Utils.toIndentedString(time)).append("\n");
-      sb.append("    rrn: ").append(Utils.toIndentedString(rrn)).append("\n");
-      sb.append("    stan: ").append(Utils.toIndentedString(stan)).append("\n");
-      sb.append("}");
-      return sb.toString();
-   }
+    @ApiModelProperty(required = false, value = "The unique transaction identifier related to this transaction. This "
+            + "transaction identifier was encoded within the QR Code and used to associate the scan and the "
+            + "payment request.")
+    @JsonProperty("tranId")
+    @NotNull
+    public String getTranId() {
+        return tranId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class PaymentConfirmation {\n");
+
+        sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
+        sb.append("    requestId: ").append(Utils.toIndentedString(requestId)).append("\n");
+        sb.append("    thirdPartyIdentifiers: ").append(Utils.toIndentedString(thirdPartyIdentifiers)).append("\n");
+        sb.append("    time: ").append(Utils.toIndentedString(time)).append("\n");
+        sb.append("    rrn: ").append(Utils.toIndentedString(rrn)).append("\n");
+        sb.append("    stan: ").append(Utils.toIndentedString(stan)).append("\n");
+        sb.append("    tranId: ").append(Utils.toIndentedString(tranId)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
 }
--- src/main/java/io/electrum/qr/api/model/PaymentReversal.java
+++ src/main/java/io/electrum/qr/api/model/PaymentReversal.java
@@ -1,8 +1,12 @@
 package io.electrum.qr.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.electrum.vas.Utils;
 import io.electrum.vas.model.BasicReversal;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Reverse a previous {@link PaymentRequest}. This may be due to a cancellation at the POS or because the original
@@ -11,18 +15,38 @@ import io.swagger.annotations.ApiModel;
 
 @ApiModel(description = "Reverse a previous {@link PaymentRequest}. This may be due to a cancellation at the POS or because the original {@link PaymentRequest} is in an unknown state.")
 public class PaymentReversal extends BasicReversal {
+    protected String tranId = null;
 
-   @Override
-   public String toString() {
-      StringBuilder sb = new StringBuilder();
-      sb.append("class PaymentReversal {\n");
+    /**
+     * The unique transaction identifier related to this transaction. This transaction identifier was encoded within the
+     * QR Code and used to associate the scan and the payment request.
+     **/
+    public PaymentReversal tranId(String tranId) {
+        this.tranId = tranId;
+        return this;
+    }
 
-      sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
-      sb.append("    requestId: ").append(Utils.toIndentedString(requestId)).append("\n");
-      sb.append("    thirdPartyIdentifiers: ").append(Utils.toIndentedString(thirdPartyIdentifiers)).append("\n");
-      sb.append("    time: ").append(Utils.toIndentedString(time)).append("\n");
-      sb.append("    reversalReason: ").append(Utils.toIndentedString(reversalReason)).append("\n");
-      sb.append("}");
-      return sb.toString();
-   }
+    @ApiModelProperty(required = false, value = "The unique transaction identifier related to this transaction. This "
+            + "transaction identifier was encoded within the QR Code and used to associate the scan and the "
+            + "payment request.")
+    @JsonProperty("tranId")
+    @NotNull
+    public String getTranId() {
+        return tranId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class PaymentReversal {\n");
+
+        sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
+        sb.append("    requestId: ").append(Utils.toIndentedString(requestId)).append("\n");
+        sb.append("    thirdPartyIdentifiers: ").append(Utils.toIndentedString(thirdPartyIdentifiers)).append("\n");
+        sb.append("    time: ").append(Utils.toIndentedString(time)).append("\n");
+        sb.append("    tranId: ").append(Utils.toIndentedString(tranId)).append("\n");
+        sb.append("    reversalReason: ").append(Utils.toIndentedString(reversalReason)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
 }
