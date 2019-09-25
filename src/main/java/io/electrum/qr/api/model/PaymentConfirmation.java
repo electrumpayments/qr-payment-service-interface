3ac7f29a6bf39cf292fac55edd0ed1a23c1c5ecb
mordi34 <mserraf34@gmail.com>
2019-09-25 13:11:08 +0200

    Add partner Institution to PaymentReversal  and PaymentConfirmation advices

--- src/main/java/io/electrum/qr/api/model/PaymentConfirmation.java
+++ src/main/java/io/electrum/qr/api/model/PaymentConfirmation.java
@@ -3,9 +3,11 @@ package io.electrum.qr.api.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.electrum.vas.Utils;
 import io.electrum.vas.model.BasicAdvice;
+import io.electrum.vas.model.Institution;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -14,39 +16,62 @@ import javax.validation.constraints.NotNull;
 
 @ApiModel(description = "Confirm that a previous {@link PaymentRequest} has completed successfully at the POS.")
 public class PaymentConfirmation extends BasicAdvice {
-    protected String tranId = null;
 
-    /**
-     * The unique transaction identifier related to this transaction. This transaction identifier was encoded within the
-     * QR Code and used to associate the scan and the payment request.
-     **/
-    public PaymentConfirmation tranId(String tranId) {
-        this.tranId = tranId;
-        return this;
-    }
+   protected Institution partner = null;
+   protected String tranId = null;
 
-    @ApiModelProperty(required = false, value = "The unique transaction identifier related to this transaction. This "
-            + "transaction identifier was encoded within the QR Code and used to associate the scan and the "
-            + "payment request.")
-    @JsonProperty("tranId")
-    @NotNull
-    public String getTranId() {
-        return tranId;
-    }
+   /**
+    * Data relating to the entity who will process the payment.
+    **/
+   public PaymentConfirmation partner(Institution partner) {
+      this.partner = partner;
+      return this;
+   }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class PaymentConfirmation {\n");
+   @ApiModelProperty(required = true, value = "Data relating to the entity who will process the payment.")
+   @JsonProperty("partner")
+   @Valid
+   @NotNull
+   public Institution getPartner() {
+      return partner;
+   }
 
-        sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
-        sb.append("    requestId: ").append(Utils.toIndentedString(requestId)).append("\n");
-        sb.append("    thirdPartyIdentifiers: ").append(Utils.toIndentedString(thirdPartyIdentifiers)).append("\n");
-        sb.append("    time: ").append(Utils.toIndentedString(time)).append("\n");
-        sb.append("    rrn: ").append(Utils.toIndentedString(rrn)).append("\n");
-        sb.append("    stan: ").append(Utils.toIndentedString(stan)).append("\n");
-        sb.append("    tranId: ").append(Utils.toIndentedString(tranId)).append("\n");
-        sb.append("}");
-        return sb.toString();
-    }
+   public void setPartner(Institution partner) {
+      this.partner = partner;
+   }
+
+   /**
+    * The unique transaction identifier related to this transaction. This transaction identifier was encoded within the
+    * QR Code and used to associate the scan and the payment request.
+    **/
+   public PaymentConfirmation tranId(String tranId) {
+      this.tranId = tranId;
+      return this;
+   }
+
+   @ApiModelProperty(required = false, value = "The unique transaction identifier related to this transaction. This "
+         + "transaction identifier was encoded within the QR Code and used to associate the scan and the "
+         + "payment request.")
+   @JsonProperty("tranId")
+   @NotNull
+   public String getTranId() {
+      return tranId;
+   }
+
+   @Override
+   public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("class PaymentConfirmation {\n");
+
+      sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
+      sb.append("    requestId: ").append(Utils.toIndentedString(requestId)).append("\n");
+      sb.append("    thirdPartyIdentifiers: ").append(Utils.toIndentedString(thirdPartyIdentifiers)).append("\n");
+      sb.append("    time: ").append(Utils.toIndentedString(time)).append("\n");
+      sb.append("    rrn: ").append(Utils.toIndentedString(rrn)).append("\n");
+      sb.append("    stan: ").append(Utils.toIndentedString(stan)).append("\n");
+      sb.append("    partner: ").append(Utils.toIndentedString(partner)).append("\n");
+      sb.append("    tranId: ").append(Utils.toIndentedString(tranId)).append("\n");
+      sb.append("}");
+      return sb.toString();
+   }
 }
--- src/main/java/io/electrum/qr/api/model/PaymentReversal.java
+++ src/main/java/io/electrum/qr/api/model/PaymentReversal.java
@@ -3,9 +3,11 @@ package io.electrum.qr.api.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.electrum.vas.Utils;
 import io.electrum.vas.model.BasicReversal;
+import io.electrum.vas.model.Institution;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 /**
@@ -15,38 +17,61 @@ import javax.validation.constraints.NotNull;
 
 @ApiModel(description = "Reverse a previous {@link PaymentRequest}. This may be due to a cancellation at the POS or because the original {@link PaymentRequest} is in an unknown state.")
 public class PaymentReversal extends BasicReversal {
-    protected String tranId = null;
 
-    /**
-     * The unique transaction identifier related to this transaction. This transaction identifier was encoded within the
-     * QR Code and used to associate the scan and the payment request.
-     **/
-    public PaymentReversal tranId(String tranId) {
-        this.tranId = tranId;
-        return this;
-    }
+   protected Institution partner = null;
+   protected String tranId = null;
 
-    @ApiModelProperty(required = false, value = "The unique transaction identifier related to this transaction. This "
-            + "transaction identifier was encoded within the QR Code and used to associate the scan and the "
-            + "payment request.")
-    @JsonProperty("tranId")
-    @NotNull
-    public String getTranId() {
-        return tranId;
-    }
+   /**
+    * Data relating to the entity who will process the payment.
+    **/
+   public PaymentReversal partner(Institution partner) {
+      this.partner = partner;
+      return this;
+   }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class PaymentReversal {\n");
+   @ApiModelProperty(required = true, value = "Data relating to the entity who will process the payment.")
+   @JsonProperty("partner")
+   @Valid
+   @NotNull
+   public Institution getPartner() {
+      return partner;
+   }
 
-        sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
-        sb.append("    requestId: ").append(Utils.toIndentedString(requestId)).append("\n");
-        sb.append("    thirdPartyIdentifiers: ").append(Utils.toIndentedString(thirdPartyIdentifiers)).append("\n");
-        sb.append("    time: ").append(Utils.toIndentedString(time)).append("\n");
-        sb.append("    tranId: ").append(Utils.toIndentedString(tranId)).append("\n");
-        sb.append("    reversalReason: ").append(Utils.toIndentedString(reversalReason)).append("\n");
-        sb.append("}");
-        return sb.toString();
-    }
+   public void setPartner(Institution partner) {
+      this.partner = partner;
+   }
+
+   /**
+    * The unique transaction identifier related to this transaction. This transaction identifier was encoded within the
+    * QR Code and used to associate the scan and the payment request.
+    **/
+   public PaymentReversal tranId(String tranId) {
+      this.tranId = tranId;
+      return this;
+   }
+
+   @ApiModelProperty(required = false, value = "The unique transaction identifier related to this transaction. This "
+         + "transaction identifier was encoded within the QR Code and used to associate the scan and the "
+         + "payment request.")
+   @JsonProperty("tranId")
+   @NotNull
+   public String getTranId() {
+      return tranId;
+   }
+
+   @Override
+   public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("class PaymentReversal {\n");
+
+      sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
+      sb.append("    requestId: ").append(Utils.toIndentedString(requestId)).append("\n");
+      sb.append("    thirdPartyIdentifiers: ").append(Utils.toIndentedString(thirdPartyIdentifiers)).append("\n");
+      sb.append("    time: ").append(Utils.toIndentedString(time)).append("\n");
+      sb.append("    tranId: ").append(Utils.toIndentedString(tranId)).append("\n");
+      sb.append("    partner: ").append(Utils.toIndentedString(partner)).append("\n");
+      sb.append("    reversalReason: ").append(Utils.toIndentedString(reversalReason)).append("\n");
+      sb.append("}");
+      return sb.toString();
+   }
 }
