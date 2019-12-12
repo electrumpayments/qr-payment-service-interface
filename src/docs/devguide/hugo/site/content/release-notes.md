39ef643361d974fe251ded933141c9a4f04ced7e
TimElectrum <timwhelan@electrum.co.za>
2019-12-12 13:48:13 +0200

    fixed hashcode and openjdk 11

--- src/docs/devguide/hugo/site/content/release-notes.md
+++ src/docs/devguide/hugo/site/content/release-notes.md
@@ -1,5 +1,17 @@
 This page describes changes to the QR Payment Service Interface implemented across different releases of the interface.
 
+## v1.2.0
+Released 12 December 2019
+
+* Updates to the Java implementation of the QR Payment Service Interface:
+    * The Java implementation is now built against OpenJDK 11.
+    * The `hashCode` method of the following classes has been corrected to not use the method from the `Object` class:
+        * `ScanNotification`
+        * `CreateQrCodeMessage`
+* The QR Payment Service Interface is now built against v3.23.0 of the base API. This includes the following changes:
+    * Added utility methods to JsonUtil class to assist in reading the contents of a file as a string and deserialising JSON objects from files.
+    * Added new field `region` to `BankAccount` model for scenarios where the `routingCode` is not sufficient to uniquely identify a bank account.
+
 ## v1.1.0
 Released 18 November 2019
 
---
+++ src/test/java/io/electrum/qr/ModelTests.java
@@ -0,0 +1,35 @@
+package io.electrum.qr;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.electrum.qr.api.model.CreateQrCodeRequest;
+import io.electrum.qr.api.model.CreateQrCodeResponse;
+import io.electrum.qr.api.model.ScanNotification;
+import io.electrum.vas.JsonUtil;
+
+public class ModelTests {
+   
+   @Test
+   public void testCreateQrCodeRequestHashcode() throws IOException {
+      CreateQrCodeRequest createQrCodeReq1 = JsonUtil.deserialize("{}", CreateQrCodeRequest.class);
+      CreateQrCodeRequest createQrCodeReq2 = JsonUtil.deserialize("{}", CreateQrCodeRequest.class);
+      Assert.assertEquals(createQrCodeReq1.hashCode(), createQrCodeReq2.hashCode());
+   }
+
+   @Test
+   public void testCreateQrCodeResponseHashcode() throws IOException {
+      CreateQrCodeResponse createQrCodeRsp1 = JsonUtil.deserialize("{}", CreateQrCodeResponse.class);
+      CreateQrCodeResponse createQrCodeRsp2 = JsonUtil.deserialize("{}", CreateQrCodeResponse.class);
+      Assert.assertEquals(createQrCodeRsp1.hashCode(), createQrCodeRsp2.hashCode());
+   }
+
+   @Test
+   public void testScanNotificationHashcode() throws IOException {
+      ScanNotification scanNotification1 = JsonUtil.deserialize("{}", ScanNotification.class);
+      ScanNotification scanNotification2 = JsonUtil.deserialize("{}", ScanNotification.class);
+      Assert.assertEquals(scanNotification1.hashCode(), scanNotification2.hashCode());
+   }
+}
